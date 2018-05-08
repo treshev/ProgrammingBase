798b6650846f36fd6a3e18e3e56903bfa27209d3
treshchev <a.treshev@gmail.com>
2018-05-08 16:46:38 +0200

    Add tests for lists with non-equals and only equal elements

--- trunk/sorting/TestSortingAlgorithm.py
+++ trunk/sorting/TestSortingAlgorithm.py
@@ -8,9 +8,16 @@ class TestBubbleSort(unittest.TestCase):
     def setUp(self):
         N = 10_000
         self.sort_algorithm = my_sort.bubble_sort
-        self.array = random.sample(range(0, N), N)
+        self.equals_array = random.sample(range(0, N), N)
+        self.array = [random.randint(0, N) for _ in range(N)]
 
-    def test_sort(self):
+    def test_sort_equals_list(self):
+        my_array = self.equals_array[:]
+        my_array.sort()
+        my_sort.insertion_sort(self.equals_array)
+        self.assertEqual(self.equals_array, my_array)
+
+    def test_sort_not_equals_list(self):
         my_array = self.array[:]
         my_array.sort()
         my_sort.insertion_sort(self.array)
@@ -19,31 +26,33 @@ class TestBubbleSort(unittest.TestCase):
     def test_specific_sort(self):
         with self.subTest("empty"):
             my_array = []
-            self.array = []
+            self.equals_array = []
             my_array.sort()
-            self.sort_algorithm(self.array)
-            self.assertEqual(self.array, my_array)
+            self.sort_algorithm(self.equals_array)
+            self.assertEqual(self.equals_array, my_array)
 
         with self.subTest("one element"):
             my_array = [random.randrange(10)]
-            self.array = my_array[:]
+            self.equals_array = my_array[:]
             my_array.sort()
-            self.sort_algorithm(self.array)
-            self.assertEqual(self.array, my_array)
+            self.sort_algorithm(self.equals_array)
+            self.assertEqual(self.equals_array, my_array)
 
 
 class TestSelectionSort(TestBubbleSort):
     def setUp(self):
         N = 10_000
         self.sort_algorithm = my_sort.selection_sort
-        self.array = random.sample(range(0, N), N)
+        self.equals_array = random.sample(range(0, N), N)
+        self.array = [random.randint(0, N) for _ in range(N)]
 
 
 class TestInsertingSort(TestBubbleSort):
     def setUp(self):
         N = 10_000
         self.sort_algorithm = my_sort.insertion_sort
-        self.array = random.sample(range(0, N), N)
+        self.equals_array = random.sample(range(0, N), N)
+        self.array = [random.randint(0, N) for _ in range(N)]
 
     if __name__ == '__main__':
         unittest.main()
